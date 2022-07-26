6ee5f3a945b80df602ff697816c847c5b2dd5698
ViniciusMendesLima <viniciusmendeslima@yahoo.com>
2022-07-25 21:02:53 -0300

    first commit

---
+++ src/modules/accounts/dtos/ICreateUserDTO.ts
@@ -0,0 +1,15 @@
+interface ICreateUserDTO {
+  name: string;
+
+  password: string;
+
+  email: string;
+
+  driver_license: string;
+
+  id?: string;
+
+  avatar?: string;
+}
+
+export { ICreateUserDTO };
---
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -0,0 +1,35 @@
+import { inject, injectable } from "tsyringe";
+
+import { AppError } from "../../../../erros/AppError";
+import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository";
+
+interface IRquest {
+  name: string;
+
+  description: string;
+}
+
+@injectable()
+class CreateSpecificationUseCase {
+  constructor(
+    @inject("SpecificationsRepository")
+    private specificationsRepository: ISpecificationsRepository
+  ) {}
+
+  async execute({ name, description }: IRquest): Promise<void> {
+    const specificationAlreadyExists =
+      await this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new AppError("Specification Already exists!");
+    }
+
+    await this.specificationsRepository.create({
+      name,
+
+      description,
+    });
+  }
+}
+
+export { CreateSpecificationUseCase };
